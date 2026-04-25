c88d945b39cfdcc0e393d165b5d09f4d27d61783
Inevitable2007tech <abhishek2007tech@gmail.com>
2026-04-25 20:13:01 +0530

    some changes

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ require('dotenv').config({ path: "cert.env" });
 const NODE_ID = process.env.NODE_ID || 'node-1';
 const MAX_BUCKET_SIZE = 10 * 1024 * 1024 * 1024;
 const MAX_FILE_SIZE = process.env.MAX_FILE_SIZE || 1 * 1024 * 1024 * 1024;
-const MAX_BUCKETS = 10;
+const MAX_BUCKETS = 12;
 const ADMIN_KEY = process.env.ADMIN_KEY || 'admin-secret-key';
 const JWT_SECRET = process.env.JWT_SECRET || 'jwt-secret-key-change-in-production';
 const MONGODB_URI = process.env.SUB_MONGODB_URI;
